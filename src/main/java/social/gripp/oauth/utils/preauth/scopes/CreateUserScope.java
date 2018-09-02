d6538b3990ec0f42612bbf8846ce12c8ea698a87
matt62king <matt62king@gmail.com>
2018-09-02 02:18:00 -0500

    initial commit

---
+++ src/main/java/social/gripp/oauth/utils/preauth/scopes/CreateUserScope.java
@@ -0,0 +1,14 @@
+package social.gripp.oauth.utils.preauth.scopes;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("#oauth2.hasScope('CREATE_USER')")
+public @interface CreateUserScope {
+}
